6a6673e2c9db3f3cf9643fbbb696d9582d87b1f4
Daquan McDaniel <daquan.mcdaniel@accesscfa.com>
2022-02-20 17:43:20 -0500

    loader commit

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+   
+    loading();
+
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     
@@ -24,7 +40,10 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
@@ -49,6 +68,7 @@ async function getQuote () {
 
  // On Load
 getQuote();
+
 
  // Event Listeners
  newQuoteBtn.addEventListener('click', getQuote);
